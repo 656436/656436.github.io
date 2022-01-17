b90c6297e47ce920188e92e11be467f692e2e29c
656436 <77640900+656436@users.noreply.github.com>
2022-01-17 19:03:32 +0300

    Add files via upload

---
+++ js/title.js
@@ -0,0 +1,42 @@
+﻿if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		loaded();
+	});
+} else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		loaded();
+	});
+}
+
+function loaded(){
+	
+	setInterval(loop, 300);
+
+}
+
+var x = 0;
+
+var titleText = [ "|",
+					"|",
+					"e |",
+					"e |",
+					"e m |",
+					"e m |",
+					"e m p |",
+					"e m p |",
+					"e m p f |",
+					"e m p f |",
+					"e m p f a |",
+					"e m p f a |",
+					"e m p f a k |",
+					"e m p f a k |",	
+					"e m p f a k e |",
+					"e m p f a k e |",
+					"e m p f a k e d ",
+					"e m p f a k e d ",				];
+
+function loop(){
+
+	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+
+}
